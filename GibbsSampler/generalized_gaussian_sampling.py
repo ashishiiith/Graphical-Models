8d48b5850c3411cd42217e63df3ed28ba22012e1
Ashish <ashish.iiith@gmail.com>
2015-04-08 11:10:43 -0400

    general WP parameter Gaussian Sampling

--- GibbsSampler/generalized_gaussian_sampling.py
+++ GibbsSampler/generalized_gaussian_sampling.py
@@ -13,8 +13,8 @@ from PIL import Image
 
 WL = None
 WP = None
-WPR = zeros([49, 50]) #Storing vertical WP weights between pixels
-WPC = zeros([50, 49]) #Storing horizontol WP weights between pixels
+WPY = zeros([49, 50]) #Storing vertical WP weights between pixels
+WPX = zeros([50, 49]) #Storing horizontol WP weights between pixels
 T  = 100 #Number of Iterations
 H  = 50  #Heights of Image
 W  = 50  #Width of Image
@@ -57,13 +57,13 @@ def compute_variance(x, y):
 
     W =  float(WL)
     if x-1 >= 0:
-        W+=WPR[x-1][y]
+        W+=WPY[x-1][y]
     if y-1 >= 0:
-        W+=WPC[x][y-1]
+        W+=WPX[x][y-1]
     if x+1<=49:
-        W+=WPR[x][y]
+        W+=WPY[x][y]
     if y+1<=49:
-        W+=WPC[x][y]
+        W+=WPX[x][y]
     
     variance = 1.0/float(2*W)
     return float(variance)
@@ -72,13 +72,13 @@ def compute_mean(x, y):
 
     Num = float(WL*xij[x][y])
     if x-1 >= 0:
-        Num+=WPR[x-1][y]*yij[x-1][y]
+        Num+=WPY[x-1][y]*yij[x-1][y]
     if y-1 >= 0:
-        Num+=WPC[x][y-1]*yij[x][y-1]
+        Num+=WPX[x][y-1]*yij[x][y-1]
     if x+1<=49:
-        Num+=WPR[x][y]*yij[x+1][y]
+        Num+=WPY[x][y]*yij[x+1][y]
     if y+1<=49:
-        Num+=WPC[x][y]*yij[x][y+1]
+        Num+=WPX[x][y]*yij[x][y+1]
     mean = float(Num)*float(compute_variance(x, y)*2)
     return mean
 
@@ -108,16 +108,16 @@ def set_general_weights():
 
     ''' Computing generalized weights of WP parameter
     '''
-    global WPR
-    global WPC
+    global WPY
+    global WPX
 
     for y in range(50):
         for x in range(49):
-            WPR[x][y] = float(WP) / float(0.01 + (xij[x][y] - xij[x+1][y])**2) 
+            WPY[x][y] = float(WP) / float(0.01 + (xij[x][y] - xij[x+1][y])**2) 
    
     for x in range(50):
         for y in range(49):
-            WPC[x][y] = float(WP) / float(0.01 + (xij[x][y] - xij[x][y+1])**2)
+            WPX[x][y] = float(WP) / float(0.01 + (xij[x][y] - xij[x][y+1])**2)
 
 def main():
  
